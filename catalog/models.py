ffca899a8936d253767d3285dfc0dfa386320c7f
nlankwei5 <lampteyebenezer220@gmail.com>
2025-09-15 12:29:51 +0000

    Create auto slug feature for category model

--- catalog/models.py
+++ catalog/models.py
@@ -1,19 +1,25 @@
 import random 
 import string
 from django.db import models
+from django.utils.text import slugify
 
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=50)
+    slug = models.SlugField(blank=True)
+
+
+    def save(self, *args, **kwargs):
+         self.slug = slugify(self.name)
+         return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    sku = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    sku = models.CharField(max_length=30, unique=True, null=True, blank=True)
     name = models.CharField(max_length= 50, blank= False)
     description = models.CharField (max_length= 100, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
