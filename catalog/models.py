81b0a5231ce6fe806cfe9e5e4ac6110ed8101741
nlankwei5 <lampteyebenezer220@gmail.com>
2025-09-15 13:19:55 +0000

    Update product model to have textfield for description

--- catalog/models.py
+++ catalog/models.py
@@ -27,7 +27,7 @@ class Category(models.Model):
 class Product(models.Model):
     sku = models.CharField(max_length=30, unique=True, null=True, blank=True)
     name = models.CharField(max_length= 50, blank= False)
-    description = models.CharField (max_length= 100, blank=True)
+    description = models.TextField (blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, validators=[validate_price])
     category = models.ForeignKey(Category, on_delete= models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
