4f90d5cdb6ef181c7532207b4ea5eb55d606f54d
nlankwei5 <lampteyebenezer220@gmail.com>
2025-09-17 13:00:14 +0000

    add validation for Proudct serializer

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -16,6 +16,15 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ['id', 'sku', 'name', 'description', 'price', 'category_name','created_at', 'updated_at']
         read_only_fields = ['id', 'sku', 'created_at', 'updated_at']
 
+    def validate_price(self, value):
+        if value > 0:
+            raise serializers.ValidationError("Price cannot be lower than 0.00")
+        return value
+    
+    def validate_name(self, value):
+        if not value:
+            raise serializers.ValidationError("Name cannot be empty")
+        return value
 
 
 
