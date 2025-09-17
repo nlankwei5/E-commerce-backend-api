40eca4bfd92315f7bb8aa3f989ef92e6c890865a
nlankwei5 <lampteyebenezer220@gmail.com>
2025-09-17 12:31:47 +0000

    Views for Category and Products

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import * 
+from rest_framework.response import Response
+from .models import * 
+
+
 
 # Create your views here.
+class CategoryViewset(viewsets.ModelViewSet):
+
+    queryset = Category.objects.all()
+    serializer_class = CatergorySerializer
+
+
+
+class ProductViewset(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Product.objects.all()
+    serializer_class = Product 
+
+    
